79d3c5cc60a53b3e6f9747e8c55bcebbd6063020
Seungyun Kim <seungyunkim95@gmail.com>
2021-07-26 21:12:39 +0900

    LambdaExpression과 LINQ 분리

--- Day3/LambdaExpression/Program.cs
+++ Day3/LambdaExpression/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace LambdaExpressionAndLINQ {
+namespace LambdaExpression {
     internal class Program {
         public static void Main(string[] args) {
             Console.WriteLine("BeforeLambda_Step1");
@@ -45,18 +44,6 @@ namespace LambdaExpressionAndLINQ {
             usingLambdaWithList.RemoveAllMethod();
             usingLambdaWithList.ForEachMethod();
             usingLambdaWithList.ConvertAllMethod();
-            Console.WriteLine();
-            
-            Console.WriteLine("LINQtoObjects");
-            LINQtoObjects linQtoObjects = new LINQtoObjects();
-            linQtoObjects.QueryExample1();
-            linQtoObjects.QueryExample2();
-            linQtoObjects.QueryExample3();
-            Console.WriteLine();
-            
-            linQtoObjects.DelayedExecution();
-            Console.WriteLine();
-            linQtoObjects.ImmediateExecution();
             Console.WriteLine();
         }
     }
@@ -253,10 +240,6 @@ namespace LambdaExpressionAndLINQ {
             // 5 이상 10 미만의 수 카운트
             var count3 = Count(numbers, n => 5 <= n && n < 10);
             Console.WriteLine(count3);
-
-            // 숫자에 '1'이 포함된 수 카운트
-            var count4 = Count(numbers, n => n.ToString().Contains('1'));
-            Console.WriteLine(count4);
         }
     }
 
@@ -314,8 +297,9 @@ namespace LambdaExpressionAndLINQ {
         // ForEach 메서드
         // 인수로 지정한 처리 내용을 리스트의 각 요소를 대상으로 실행
         public void ForEachMethod() {
-            // 아래 세 줄으 코드는 동일한 내용
+            // 아래의 코드는 동일한 내용
             list.ForEach(s => Console.WriteLine(s));
+            Console.WriteLine();
             foreach (var s in list)
                 Console.WriteLine(s);
             Console.WriteLine();
@@ -327,76 +311,6 @@ namespace LambdaExpressionAndLINQ {
         public void ConvertAllMethod() {
             var lowerList = list.ConvertAll(s => s.ToLower());
             lowerList.ForEach(s => Console.WriteLine(s));
-        }
-    }
-
-    /// <summary>
-    /// LINQ to Objects
-    /// </summary>
-
-    // LINQ를 사용하여 객체, 데이터, XML과 같은 다양한 데이터를 표준화된 방법으로 처리할 수 있음
-    class LINQtoObjects {
-        List<string> names = new List<string> {
-            "Seoul", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
-        };
-
-        // Where 메서드는 시퀀스에서 조건을 만족하는 것만 추출함
-        // 배열, List, Dictionary 등 IEnumerable 인터페이스를 구현하고 있는 형에는 Where 메서드 이용 가능
-        // 도시 이름 글자 수가 5자 이하 출력
-        public void QueryExample1() {
-            IEnumerable<string> query = names.Where(s => s.Length <= 5);
-            foreach (string s in query)
-                Console.WriteLine(s);
-        }
-
-        // Where 메서드를 통해 추출한 문자 도시 이름을 모두 소문자로 변환 후 출력
-        public void QueryExample2() {
-            var query = names.Where(s => s.Length <= 5).Select(s => s.ToLower());
-            foreach (string s in query)
-                Console.WriteLine(s);
-        }
-
-        // names에 저장되어 있는 문장열의 길이 열거
-        public void QueryExample3() {
-            var query = names.Select(s => s.Length);
-            foreach (var n in query)
-                Console.Write("{0} ", n);
-            Console.WriteLine();
-        }
-
-
-        // 지연실행
-        // Where 메서드의 반환값을 query 변수에 대입한 후에 names 배열의 요소 수정
-        // query 변수에는 검색된 결과가 대입되지 않음 -> 필요할 때 쿼리가 실행됨
-        public void DelayedExecution() {
-            string[] names = {
-                "Seoul", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra"
-            };
-            var query = names.Where(s => s.Length <= 5);
-            foreach (var item in query)
-                Console.WriteLine(item);
-            Console.WriteLine("------");
-
-            names[0] = "Busan";
-            foreach (var item in query)
-                Console.WriteLine(item);
-        }
-
-        // 즉시 실행
-        // ToArray 메서드가 호출되었을 때 쿼리가 실행되며 결과가 배열에 저장됨
-        // ToArray 대신 ToList 메서드로 바꿔도 동일한 결과
-        public void ImmediateExecution() {
-            string[] names = {
-                "Seoul", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra"
-            };
-            var query = names.Where(s => s.Length <= 5).ToArray(); // 배열로 변환
-            foreach (var item in query)
-                Console.WriteLine(item);
-            Console.WriteLine("------");
-
-            names[0] = "Busan";
-            foreach (var item in query)
-                Console.WriteLine(item);
         }
     }
 }
--- Day3/LambdaExpression/Properties/AssemblyInfo.cs
+++ Day3/LambdaExpression/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LambdaExpressionAndLINQ")]
+[assembly: AssemblyTitle("LambdaExpression")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LambdaExpressionAndLINQ")]
+[assembly: AssemblyProduct("LambdaExpression")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
